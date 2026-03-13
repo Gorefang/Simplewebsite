4752dae3dedf19fa3974f613f3a04fb08294116c
Gorefang <125388201+Gorefang@users.noreply.github.com>
2026-03-13 10:56:58 +0000

    Update quotes and event titles in SimpleWebSite.js

--- SimpleWebSite.js
+++ SimpleWebSite.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
+document.addEventListener("DOMContentLoaded", function() {
+    var calendarEl = document.getElementById("calendar");
     if(!calendarEl){
-        console.error('Calendar element not found!');
+        console.error("Calendar element not found!");
     return;
     }
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
+        initialView: "dayGridMonth",
         height: "parent",
         headerToolbar: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth"   /* can add more options like so:"dayGridMonth,timeGridWeek,timeGridDay" */
         },
         events: [
-            { title: 'My Event', start: '2025-07-01' },
-            { title: 'Another Event', start: '2025-07-15', end: '2025-07-17' }
+            { title: "My Event", start: "2025-07-01" },
+            { title: "My Even 2", start: "2025-07-15", end: "2025-07-17" }
         ]
     });
     calendar.render();
